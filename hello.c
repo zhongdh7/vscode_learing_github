8ec5b3e6653af85b673c129411cb32e16c49ea46
zhongdh7 <zhongdh7@mail2.sysu.edu.cn>
2026-03-13 22:49:15 +0800

    8commit

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("hello world\n");
+    printf("这是一个使用C17语言标准版本的项目！\n");
+    return 0;
+}
--- hello_world.cpp
+++ hello_world.cpp
@@ -4,5 +4,6 @@ int main()
 {
     cout<<"Hello World!"<<endl;
     cout<<"这个是一个C++17版本的Hello World程序！"<<endl;
+    cout<<"这是一个使用C++17语言标准版本的项目！"<<endl;
     return 0;
 }
