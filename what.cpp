d3a8f9847e3f1eaabf1f156ebe6b7396c4c31cc4
zhongdh7 <zhongdh7@mail2.sysu.edu.cn>
2026-03-13 22:56:56 +0800

    10commit

--- what.cpp
+++ what.cpp
@@ -5,5 +5,9 @@ int main()
     cout<<"Hello World!"<<endl;
     cout<<"这个是一个C++17版本的Hello World程序！"<<endl;
     cout<<"这是一个使用C++17语言标准版本的项目！"<<endl;
+    for(int i=0;i<9;i++)
+    {
+        cout<<"这是第"<<i+1<<"行！"<<endl;
+    }
     return 0;
 }
