c67fd06bdb52e24fdef96c619fb46929c5be15a3
zhongdh7 <zhongdh7@mail2.sysu.edu.cn>
2026-03-13 22:01:27 +0800

    third_commit

--- first.py
+++ first.py
@@ -7,4 +7,13 @@ import sympy as sp
 x = sp.symbols('x')
 f = sp.sin(x)
 integral = sp.integrate(f, (x, 0, sp.pi))
-print(integral)
+print("this is the integral of sin(x) from 0 to pi:", integral)
+for i in range(5):
+    print(i)
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+print("the factorial of 5 is:", factorial(5))
+
