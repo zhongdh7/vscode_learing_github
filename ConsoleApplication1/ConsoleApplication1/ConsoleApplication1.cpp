b65a640d6dbcf77653fb6c0f63690562ca1f42c4
zhongdh7 <zhongdh7@mail2.sysu.edu.cn>
2026-03-17 16:32:49 +0800

    what

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,41 @@
 ﻿#include<iostream>
 using namespace std;
+const int N = 1e5 + 10;
+int a[N] = {};
+
+bool flag = false, first = true;
 int main()
 {
-	cout<<"Hello World!"<<endl;
+	int n;
+	cin >> n;
+	int target;
+	int begin, end;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	cin >> target;
+	for (int i = 0; i < n; i++)
+	{
+		if (target == a[i])
+		{
+			if (first)
+			{
+				begin = i;
+				end = i;
+				flag = true;
+			}
+			else
+			{
+				end = i;
+			}
+		}
+	}
+	if (flag)
+	{
+		first = -1;
+		end = -1;
+	}
+	cout << first << ' ' << endl;
 	return 0;
 }
